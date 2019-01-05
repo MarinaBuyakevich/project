bdc9314865c70afc5991ec68983e59f61d528a21
MarinaBuyakevich <manyunya1011@gmail.com>
2019-01-05 17:18:59 +0200

    new form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
-  title = 'project';
+  button_1 = false;
+  button_2 = false;
+  button_3 = false;
+  button_4 = false;
+  button_5 = false;
+  button_6 = false;
 }
